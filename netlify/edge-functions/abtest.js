912796bddec8f35a857245142b968cef906ec673
Jordan Whistler <whistler.5@osu.edu>
2022-06-03 18:13:56 -0400

    ab test test

--- netlify/edge-functions/abtest.js
+++ netlify/edge-functions/abtest.js
@@ -1,4 +1,35 @@
-import { VARIANTS, RULE } from './abconfig';
+const VARIANTS = [
+  {
+    name: 'main',
+    url: 'https://main--jocular-pavlova-6e97f0.netlify.app/',
+    script: '',
+  },
+  {
+    name: 'alt',
+    url: 'https://deploy-preview-1--jocular-pavlova-6e97f0.netlify.app/',
+    script: '',
+  },
+];
+const RULE = {
+  filter: {
+    device: '',
+    browser: [],
+    header: {},
+    cookie: '',
+  },
+  destinations: [
+    {
+      variantName: 'main',
+      weight: '50',
+    },
+    {
+      variantName: 'alt',
+      weight: '50',
+    },
+  ],
+  note: '',
+};
+
 const abConfig = { VARIANTS, RULE };
 
 export default (request) => {
